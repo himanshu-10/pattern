628486d94710c5d668957cfa77eaa653714f48ed
himanshu-10 <himanshusrivastava5390@gmail.com>
2023-07-01 16:24:49 +0530

    pattern1

---
+++ pattern.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern1(int n){
+    for(int i = 0 ; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern2(int n){
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j <= i ; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern3(int n){
+     for(int i = 1; i < n ; i++){
+        for(int j = 1; j <= i ; j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+void pattern4(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+void pattern5(int n){
+    for(int i = 1; i<= n; i++){
+        for(int j =1 ; j<= n-i+1; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    // pattern1(n);
+    // pattern2(n);
+    // pattern3(n);
+
+    
+    // 1
+    // 22
+    // 333
+    // 4444
+    // pattern4(n);
+
+    // * * * * 
+    // * * *
+    // * *
+    // * 
+    // pattern5(n);
+}
