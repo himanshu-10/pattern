18e3939d29c8f535b908bd7a09a9e6a4e87e3889
himanshu-10 <himanshusrivastava5390@gmail.com>
2023-07-03 01:49:47 +0530

    second

--- pattern.cpp
+++ pattern.cpp
@@ -105,6 +105,84 @@ void pattern10(int n){
     }
 }
 
+void pattern11(int n){
+    for(int i = 0; i<n;i++){
+        int start = 1;
+        if(i % 2 == 0){
+            start = 1;
+        }else{
+            start = 0;
+        }
+
+        for(int j = 0 ;j<=i; j++){
+            cout << start;
+            start = !start;
+        }
+        cout << endl;
+    }
+
+}
+
+void pattern12(int n){
+    for(int i = 1; i<= n; i++){
+        // number 
+        for(int j = 1; j<=i; j++){
+            cout << j;
+        }
+
+        // space
+        for(int j = 1; j<= 2*(n-i);j++){
+            cout << " ";
+        }
+
+        //number
+        for(int j = i; j>=1; j--){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+void pattern13(int n){
+    int num =1;
+    for(int i = 1; i<=n;i++){
+        for(int j = 1; j<=i; j++){
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+}
+
+void pattern14(int n){
+    for(int i = 0; i< n;i++){
+        for(char ch = 'A'; ch <= 'A'+(n-i-1); ch++){
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+void pattern15(int n){
+    for(int i = 0; i<n; i++){
+        for(char ch = 'A' ; ch <= 'A'+i; ch++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern16(int n){
+    for(int i = 0; i<n;i++){
+        char ch = 'A'+i;
+        for(int j = 0 ; j<=i;j++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+
 int main(){
     int n;
     cout << "enter number" << endl;
@@ -195,4 +273,46 @@ int main(){
     // **
     // *
     // pattern10(n);
+
+    // 1
+    // 01
+    // 101
+    // 0101
+    // 10101
+    // pattern11(n);
+
+
+    // 1        1
+    // 12      21
+    // 123    321
+    // 1234  4321
+    // 1234554321
+    // pattern12(n);
+
+
+    // 1 
+    // 2 3
+    // 4 5 6
+    // 7 8 9 10
+    // 11 12 13 14 15
+    // pattern13(n);
+
+
+    // A 
+    // A B
+    // A B C
+    // A B C D
+    // A B C D E
+    // pattern15(n);
+
+
+    // ABCDE
+    // ABCD
+    // ABC
+    // AB
+    // A
+    // pattern14(n);
+
+    pattern16(n);
+
 }
